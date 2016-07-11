ae38a1520cc81ded938ed78916e5a97b42dd245e
Kristjan Gannon <gannonk08@gmail.com>
2016-07-11 12:54:14 -0600

    updated problem to stop before 1000

--- JavascriptLoops.js
+++ JavascriptLoops.js
@@ -54,7 +54,7 @@ for (var i = 1; i < 51; i++) {
 }
 //to sum the multiples of 3 and 5 under 1000.
 var sum = 0;
-for (var i = 1; i < 1001; i++) {
+for (var i = 1; i < 1000; i++) {
     if ((i % 3) === 0) {
         sum = sum + i;
     } else if ((i % 5) === 0) {
